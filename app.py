c5f045771cfd3e9cf57c96f71754bd2399de120b
RohanWaje <67456227+RohanWaje@users.noreply.github.com>
2020-07-10 11:15:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -365,5 +365,5 @@ def predict():
 
 
 if __name__ == "__main__":
-    app.run(host='127.0.0.1', port=8001, debug=True)
-    #app.run(debug=True) -- Heroku
+    #app.run(host='127.0.0.1', port=8001, debug=True)
+    app.run(debug=True)
